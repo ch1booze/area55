37ac17d73b409a7af036bd908a54c299bc1e9fd3
Victor Momodu <victoramomodu@gmail.com>
2025-04-20 18:27:49 +0100

    feat: add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AiModule } from './ai/ai.module';
+import { MessagingModule } from './messaging/messaging.module';
+import { UsersModule } from './users/users.module';
+import { RemindersModule } from './reminders/reminders.module';
 
 @Module({
-  imports: [],
+  imports: [AiModule, MessagingModule, UsersModule, RemindersModule],
   controllers: [AppController],
   providers: [AppService],
 })
