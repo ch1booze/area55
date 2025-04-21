137cb8afec89f7ac462cb9439c161241847d61ec
Victor Momodu <victoramomodu@gmail.com>
2025-04-21 16:34:28 +0100

    feat: add database resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AiModule } from './ai/ai.module';
 import { MessagingModule } from './messaging/messaging.module';
 import { UsersModule } from './users/users.module';
 import { RemindersModule } from './reminders/reminders.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AiModule, MessagingModule, UsersModule, RemindersModule],
+  imports: [AiModule, MessagingModule, UsersModule, RemindersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
