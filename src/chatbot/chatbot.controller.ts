697d6f5bb350a3a4358e86c4e10cd4eb086e7506
Victor Momodu <victoramomodu@gmail.com>
2025-05-09 12:45:29 +0100

    feat: reimplement webhook

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -1,19 +1,39 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { ChatbotService } from './chatbot.service';
 
 @Controller('chatbot')
 export class ChatbotController {
   constructor(private readonly chatbotService: ChatbotService) {}
 
+  @Get('webhook')
+  verifyWebhook(
+    @Query('hub.mode') mode: string,
+    @Query('hub.verify_token') token: string,
+    @Query('hub.challenge') challenge: string,
+    @Res() res: Response,
+  ) {
+    const VERIFY_TOKEN = 'token';
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(400);
+    }
+  }
+
   @Post('webhook')
   @HttpCode(200)
   async handleIncomingMessageWebhook(@Body() body: any) {
-    const message = body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
-    const phoneNumber = message?.from;
-    if (phoneNumber && message?.type === 'text') {
-      await this.chatbotService.sendMessage(phoneNumber, 'Hi, I am Arial');
-    }
-
+    console.log('Incoming webhook:', JSON.stringify(body, null, 2));
     return 'EVENT_RECEIVED';
   }
 }
