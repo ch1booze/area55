ca3d2c3268d74894636442f201ce14a365f6f878
Victor Momodu <victoramomodu@gmail.com>
2025-05-09 19:09:39 +0100

    fix: resolve deps

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
+import { ChatbotModule } from 'src/chatbot/chatbot.module';
 
 @Module({
+  imports: [ChatbotModule],
   providers: [CronService],
 })
 export class CronModule {}
